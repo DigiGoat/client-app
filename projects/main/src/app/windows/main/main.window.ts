b08174080f219ce89661a47d41e0ed4727897d7a
Kolton Bloom <bloomkd46@icloud.com>
2024-06-08 09:42:57 -0700

    Reconfigured Windows to extend a base

--- projects/main/src/app/windows/main/main.window.ts
+++ projects/main/src/app/windows/main/main.window.ts
@@ -1,31 +1,7 @@
-import { app, BrowserWindow } from 'electron';
-import { join } from 'path';
+import { Window } from '../window';
 
-export class MainWindow {
-  base = join(__dirname, '../../../../');
+export class MainWindow extends Window {
   constructor() {
-    this.createWindow();
-  }
-  private window?: BrowserWindow;
-  createWindow() {
-    this.window = new BrowserWindow({
-      show: false,
-      minWidth: 992, //Minimum width for the bootstrap menubar
-      webPreferences: {
-        preload: join(this.base, 'preload/bundle.js')
-      }
-    });
-
-    const startURL = app.isPackaged ? `file://${join(this.base, 'renderer/browser', 'index.html')}` : 'http://localhost:4200';
-
-    this.window.loadURL(startURL);
-    this.window.on('ready-to-show', () => {
-      this.show();
-    });
-  }
-  show() {
-    this.window?.show();
-    this.window?.setSize(992, 600);
-    this.window?.center();
+    super({ minWidth: 992, height: 600 });
   }
 }
---
+++ projects/main/src/app/windows/window.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow, type BrowserWindowConstructorOptions } from 'electron';
+import { join } from 'path';
+
+export class Window {
+  private base = join(__dirname, '../../../');
+  protected window?: BrowserWindow;
+  constructor(options?: BrowserWindowConstructorOptions, path?: string) {
+    this.window = new BrowserWindow({
+      show: false,
+      webPreferences: {
+        preload: join(this.base, 'preload/bundle.js')
+      },
+      ...options
+    });
+
+    const startURL = app.isPackaged ? `file://${join(this.base, 'renderer/browser', 'index.html')}#${path ?? ''}` : `http://localhost:4200/#/${path ?? ''}`;
+
+    this.window.loadURL(startURL);
+    this.window.on('ready-to-show', () => {
+      this.window?.show();
+      this.window?.setSize(options.width ?? options.minWidth ?? options.maxWidth ?? -1, options.height ?? options.minHeight ?? options.maxHeight ?? -1);
+      this.window?.center();
+    });
+  }
+}
