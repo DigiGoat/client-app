5053bf81b759f0439c3d7dbc822a4f91b75aa220
Kolton Bloom <bloomkd46@icloud.com>
2024-06-17 09:25:40 -0700

    Updated suggestion directive to trigger change detection it also works for textarea's now

--- projects/renderer/src/app/directives/suggestion/suggestion.directive.ts
+++ projects/renderer/src/app/directives/suggestion/suggestion.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: 'input[suggestion]'
+  selector: 'input[suggestion], textarea[suggestion]'
 })
 export class SuggestionDirective {
   /** What to display as a placeholder and autofill if selected */
@@ -12,7 +12,8 @@ export class SuggestionDirective {
   @HostListener('focus') onFocus() {
     if (!this.el.nativeElement.value.length) {
       this.el.nativeElement.value = this.suggestion;
+      this.el.nativeElement.dispatchEvent(new Event('input'));
     }
   }
-  constructor(private el: ElementRef<HTMLInputElement>) { }
+  constructor(private el: ElementRef<HTMLInputElement | HTMLTextAreaElement>) { }
 }
