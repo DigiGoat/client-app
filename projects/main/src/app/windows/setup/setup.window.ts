172008c08d052ebff7226c6f5f7d81521bec9a48
Kolton Bloom <bloomkd46@icloud.com>
2024-06-23 22:41:34 -0400

    Some minor updates & improvements

--- projects/main/src/app/windows/setup/setup.window.ts
+++ projects/main/src/app/windows/setup/setup.window.ts
@@ -1,14 +1,11 @@
-import { BrowserWindow } from 'electron';
 import { MainWindow } from '../main/main.window';
 import { Window } from '../window';
 
 export class SetupWindow extends Window {
   constructor() {
-    super({ resizable: false, width: 500, height: 500, title: 'Setup' }, 'setup');
+    super({ resizable: false, width: 500, height: 500, title: 'Setup', fullscreen: false }, 'setup');
     this.window.on('closed', () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        new MainWindow();
-      }
+      new MainWindow();
     });
   }
 }
