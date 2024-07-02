44f3d80b9d3320e967c528be2eab0af0b8e02e13
Kolton Bloom <bloomkd46@icloud.com>
2024-07-02 13:57:28 -0700

    Added error parsing

--- projects/renderer/src/app/services/adga/adga.service.ts
+++ projects/renderer/src/app/services/adga/adga.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
+import { DialogService } from '../dialog/dialog.service';
+import { WindowService } from '../window/window.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ADGAService {
 
-  constructor() { }
+  constructor(private dialogService: DialogService, private windowService: WindowService) { }
+
+  async handleError(err: Error, title: string) {
+    if (err.message.includes('No ADGA Account Found!')) {
+      const action = await this.dialogService.showMessageBox({ message: title, detail: 'Please login to use this feature', buttons: ['Login', 'Cancel'], type: 'warning' });
+      if (action.response === 0) {
+        await this.windowService.openLogin();
+      }
+    } else if (err.message.includes('Invalid Login ID Or Password')) {
+      const action = await this.dialogService.showMessageBox({ message: title, detail: 'Invalid Login Credentials. Please Log Back In & Try Again', buttons: ['Login', 'Cancel'], type: 'warning' });
+      if (action.response === 0) {
+        await this.windowService.openLogin();
+      }
+
+    } else {
+      await this.dialogService.showMessageBox({ message: title, detail: err.message, type: 'error' });
+    }
+  }
 
   getAccount = window.electron.adga.getAccount;
   login = window.electron.adga.login;
