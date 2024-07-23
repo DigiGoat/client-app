80d240c5066b8633e11f5b64cbca15cf8f90e9b4
Kolton Bloom <bloomkd46@icloud.com>
2024-07-23 13:47:56 -0700

    Improved the online-only directive reliability

--- projects/renderer/src/app/directives/online-only/online-only.directive.ts
+++ projects/renderer/src/app/directives/online-only/online-only.directive.ts
@@ -1,30 +1,28 @@
-import { ChangeDetectorRef, Directive, ElementRef, HostListener, type OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, type OnInit } from '@angular/core';
 
 @Directive({
   selector: '[online-only]'
 })
 export class OnlineOnlyDirective implements OnInit {
   @HostListener('window:offline') onOffline() {
-    this.el.nativeElement.setAttribute('was-disabled', String(this.el.nativeElement.disabled));
-    this.el.nativeElement.parentElement?.setAttribute('old-tooltip', this.el.nativeElement.parentElement!.getAttribute('tooltip') || '');
+    this.disabled = true;
     if (this.el.nativeElement.classList.contains('show')) {
       this.el.nativeElement.click();
     }
-    this.el.nativeElement.disabled = true;
-    this.el.nativeElement.parentElement?.setAttribute('tooltip', 'Internet Required');
   }
   @HostListener('window:online') onOnline() {
-    this.el.nativeElement.disabled = this.el.nativeElement.getAttribute('was-disabled') === 'true';
-    this.el.nativeElement.parentElement?.setAttribute('tooltip', this.el.nativeElement.parentElement!.getAttribute('old-tooltip') || '');
+    this.disabled = false;
   }
-  constructor(private el: ElementRef<HTMLButtonElement>, private cdr: ChangeDetectorRef) { }
+  constructor(private el: ElementRef<HTMLButtonElement>) { }
   ngOnInit() {
-    this.el.nativeElement.setAttribute('was-disabled', String(this.el.nativeElement.disabled));
-    this.el.nativeElement.parentElement?.setAttribute('old-tooltip', this.el.nativeElement.parentElement!.getAttribute('tooltip') || '');
-    this.el.nativeElement.disabled = !navigator.onLine;
-    if (!navigator.onLine) {
-      this.el.nativeElement.parentElement?.setAttribute('tooltip', 'Internet Required');
+    this.disabled = !navigator.onLine;
+  }
+  set disabled(disabled: boolean) {
+    this.el.nativeElement.disabled = disabled;
+    if (disabled) {
+      this.el.nativeElement.classList.add('disabled');
+    } else {
+      this.el.nativeElement.classList.remove('disabled');
     }
-    this.cdr.detectChanges();
   }
 }
