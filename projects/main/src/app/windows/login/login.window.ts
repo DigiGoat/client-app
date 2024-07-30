c1ce3467d7962ee7d50be124ca353fb555f2665c
Kolton Bloom <bloomkd46@icloud.com>
2024-07-30 15:07:34 -0700

    fixed a windows bug for the login window

--- projects/main/src/app/windows/login/login.window.ts
+++ projects/main/src/app/windows/login/login.window.ts
@@ -5,9 +5,16 @@ import { Window } from '../window';
 export class LoginWindow extends Window {
   constructor() {
     super({ resizable: false, width: 500, height: 500, title: 'ADGA Login', fullscreen: false }, 'login');
-    app.setSecureKeyboardEntryEnabled(true);
-    this.window.on('closed', () => {
-      app.setSecureKeyboardEntryEnabled(false);
+    try {
+      app.setSecureKeyboardEntryEnabled(true);
+    } catch (e) {
+      //Do nothing, this just means we aren't on Mac
+    } this.window.on('closed', () => {
+      try {
+        app.setSecureKeyboardEntryEnabled(false);
+      } catch (e) {
+        //Do nothing, this just means we aren't on Mac
+      }
       if (BrowserWindow.getAllWindows().length === 0) {
         new MainWindow();
       }
