3800ce213670b2338795396d3caffe75f401e04c
Kolton Bloom <bloomkd46@icloud.com>
2024-07-30 13:36:14 -0700

    Improved git guard to work better on mac

--- projects/renderer/src/app/guards/git/git.guard.ts
+++ projects/renderer/src/app/guards/git/git.guard.ts
@@ -8,14 +8,26 @@ export const GitGuard: CanActivateFn = async () => {
   const gitService = inject(GitService);
   const windowService = inject(WindowService);
   const dialogService = inject(DialogService);
-  const version = await gitService.version();
-  if (version.installed) {
-    return true;
-  } else {
-    console.warn('Git Not Found!');
-    await dialogService.showMessageBox({ message: 'Git Not Found!', type: 'warning', detail: 'Please Click Below To Setup Git' });
-    await windowService.openGit();
-    await windowService.close();
+
+  try {
+    const version = await gitService.version();
+    if (version.installed) {
+      return true;
+    } else {
+      console.warn('Git Not Found!');
+      await dialogService.showMessageBox({ message: 'Git Not Found!', type: 'warning', detail: 'Please Click Below To Setup Git' });
+      await windowService.openGit();
+      await windowService.close();
+      return false;
+    }
+  } catch (e) {
+    const message = (e as Error).message;
+    if (message.includes('No developer tools were found, requesting install.')) {
+      await dialogService.showMessageBox({ message: 'Git Not Found!', type: 'warning', detail: 'Click OK To Quit, Then Open "Install Command Line Developer Tools" In Your Dock And Follow The Steps. Once Complete, Re-Open The DigiGoat' });
+      await windowService.quit();
+    } else {
+      await dialogService.showMessageBox({ message: 'Git Error!', type: 'error', detail: message });
+    }
     return false;
   }
 };
