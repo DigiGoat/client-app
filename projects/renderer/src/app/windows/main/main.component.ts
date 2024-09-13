c72108d27faf7a3acaba217b5d7fe51b56a54684
Kolton Bloom <bloomkd46@icloud.com>
2024-09-13 09:25:12 -0700

    improved the progress bar

--- projects/renderer/src/app/windows/main/main.component.ts
+++ projects/renderer/src/app/windows/main/main.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ViewEncapsulation, type OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, signal, ViewEncapsulation, type OnInit } from '@angular/core';
 import { DialogService } from '../../services/dialog/dialog.service';
 import { GitService } from '../../services/git/git.service';
 import { WindowService } from '../../services/window/window.service';
@@ -11,26 +11,7 @@ import { WindowService } from '../../services/window/window.service';
 })
 export class MainComponent implements OnInit {
   changes = 0;
-  constructor(private gitService: GitService, private cdr: ChangeDetectorRef, private dialogService: DialogService, private windowService: WindowService) {
-    this.gitService.onprogress = (event) => {
-      if (event.method === 'push') {
-        switch (event.stage) {
-          case 'counting':
-            this.publishProgress = event.progress / 25;
-            break;
-          case 'compressing':
-            this.publishProgress = 25 + event.progress / 25;
-            break;
-          case 'writing':
-            this.publishProgress = 50 + event.progress / 25;
-            break;
-          case 'remote:':
-            this.publishProgress = 75 + event.progress / 25;
-            break;
-        }
-      }
-    };
-  }
+  constructor(private gitService: GitService, private cdr: ChangeDetectorRef, private dialogService: DialogService, private windowService: WindowService) { }
 
   async ngOnInit() {
     this.changes = (await this.gitService.getStatus()).ahead;
@@ -38,19 +19,36 @@ export class MainComponent implements OnInit {
       this.changes = (await this.gitService.getStatus()).ahead;
       this.cdr.detectChanges();
     };
+    this.gitService.onprogress = (event) => {
+      if (event.method === 'push') {
+        switch (event.stage) {
+          case 'counting':
+            this.publishProgress.set(event.progress / 25);
+            break;
+          case 'compressing':
+            this.publishProgress.set(25 + event.progress / 25);
+            break;
+          case 'writing':
+            this.publishProgress.set(50 + event.progress / 25);
+            break;
+          case 'remote:':
+            this.publishProgress.set(75 + event.progress / 25);
+            break;
+        }
+      }
+    };
   }
   publishing = false;
-  publishProgress = 0;
+  publishProgress = signal(0);
   async publish() {
     this.publishing = true;
+    this.publishProgress.set(5);
     if ((await this.gitService.getSetup()).token) {
       try {
         await this.gitService.push();
       } catch (err) {
         console.warn(err);
         await this.gitService.handleError('Publish Failed!', err as Error);
-      } finally {
-        this.publishProgress = 0;
       }
     } else {
       const action = await this.dialogService.showMessageBox({ message: 'No Access Token Configured!', type: 'warning', detail: 'Please Configure A Access Token Before Publishing', buttons: ['Open Setup', 'Cancel'] });
@@ -60,6 +58,7 @@ export class MainComponent implements OnInit {
       }
     }
     this.publishing = false;
+    this.publishProgress.set(0);
   }
   async reset() {
     const action = await this.dialogService.showMessageBox({ message: 'Are you sure you want to reset?', detail: 'This will permanently reset ALL unpublished changes. This cannot be undone', type: 'warning', buttons: ['Reset Changes', 'Cancel'] });
