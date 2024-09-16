697fec73c1775127e12deb743cff56ed90fc219b
Kolton Bloom <bloomkd46@icloud.com>
2024-09-15 17:31:31 -0700

    fixed env variable mixup

--- scripts/check.ts
+++ scripts/check.ts
@@ -38,12 +38,12 @@ let success = true;
 const summary = ['## Pre Check Summary:', ''];
 
 async function checkBranch() {
-  if (process.env['GITHUB_HEAD_REF'] === 'main' && process.env['GITHUB_REF_NAME'] !== 'beta') {
+  if (process.env['GITHUB_HEAD_REF'] === 'main' && process.env['GITHUB_BASE_REF'] !== 'beta') {
     log.error('Only the beta branch may merge into the main branch!');
-    summary.push(`- [ ] Branch Check: Only the beta branch may merge into the main branch! (${process.env['GITHUB_HEAD_REF']} === 'main' && ${process.env['GITHUB_REF_NAME']} !== 'beta')`);
+    summary.push(`- [ ] Branch Check: Only the beta branch may merge into the main branch! (\`${process.env['GITHUB_HEAD_REF']} === 'main' && ${process.env['GITHUB_BASE_REF']} !== 'beta')\``);
     success = false;
   } else {
-    summary.push(`- [x] Branch Check: Branches are compatible (${process.env['GITHUB_HEAD_REF']} --> ${process.env['GITHUB_REF_NAME']})`);
+    summary.push(`- [x] Branch Check: Branches are compatible (\`${process.env['GITHUB_HEAD_REF']} --> ${process.env['GITHUB_BASE_REF']}\`)`);
   }
 }
 async function checkVersion() {
