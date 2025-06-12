ce139c5851af826f771839f873d824094544c42c
Kolton Bloom <bloomkd46@icloud.com>
2025-06-12 13:42:44 -0700

    updated the fix-version support to also update the adga dependency

--- scripts/version.ts
+++ scripts/version.ts
@@ -2,4 +2,5 @@ import { readJSONSync, writeJSONSync } from 'fs-extra';
 
 const packageJson = readJSONSync('package.json');
 packageJson.version = packageJson.version.split('-')[0];
+packageJson.dependencies.adga = packageJson.dependencies.adga.split('-')[0];
 writeJSONSync('package.json', packageJson, { spaces: 2 });
