8a42da41e716f6cc5383e641dfe27aeaa2c1c213
Kolton Bloom <75853497+bloomkd46@users.noreply.github.com>
2026-06-13 14:21:49 -0700

    updated eslint

--- projects/preload/src/app/app/app.module.ts
+++ projects/preload/src/app/app/app.module.ts
@@ -32,7 +32,5 @@ export class AppModule {
   };
   constructor() {
     contextBridge.exposeInMainWorld('electron', this.api);
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    //(require('@sentry/electron/preload-namespaced') as typeof Sentry).hookupIpc();
   }
 }
