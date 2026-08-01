f5c4e07ace17017a30c3b9eb4529cfd434cef92b
Kolton Bloom <75853497+bloomkd46@users.noreply.github.com>
2026-08-01 13:15:55 -0700

    switched the history page to use signals

--- projects/renderer/src/app/windows/main/history/history.component.ts
+++ projects/renderer/src/app/windows/main/history/history.component.ts
@@ -1,4 +1,4 @@
-import { Component, type OnInit, ChangeDetectionStrategy, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, type OnInit, type WritableSignal } from '@angular/core';
 import type { History } from '../../../../../../shared/services/git/git.service';
 import { GitService } from '../../../services/git/git.service';
 import { MarkedService } from '../../../services/marked/marked.service';
@@ -7,23 +7,31 @@ import { MarkedService } from '../../../services/marked/marked.service';
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
 export class HistoryComponent implements OnInit {
   private gitService = inject(GitService);
   marked = inject(MarkedService);
 
-  history?: History;
+  history: WritableSignal<History | undefined> = signal(undefined);
   async ngOnInit() {
-    this.history = await this.gitService.getHistory();
+    this.history.set(await this.gitService.getHistory());
 
-    this.gitService.onchange = async () => this.history = await this.gitService.getHistory();
+    this.gitService.onchange = async () => this.history.set(await this.gitService.getHistory());
   }
   formatBody(body: string) {
     body = body.replace(/"([^"]*)"/g, (match, p1) => `<span class="unsaved">"${this.marked.parseInline(p1.replaceAll('\\n', '\n'))}"</span>`);
     body = body.replace(/\b(\d+(\.\d+)?)\b/g, (match) => `<span class="text-info-emphasis">${match}</span>`);
     body = body.replace(/\b(true|false)\b/g, (match) => `<span class="text-danger-emphasis">${match}</span>`);
     return body;
+  }
+
+  briefBodyFormat(body: string) {
+    body = body.replace(/"([^"]*)"/g, (match, p1) => `<span class="unsaved">"${p1.replaceAll('\\n', '\n')}"</span>`);
+    body = body.replace(/\b(\d+(\.\d+)?)\b/g, (match) => `<span class="text-info-emphasis">${match}</span>`);
+    body = body.replace(/\b(true|false)\b/g, (match) => `<span class="text-danger-emphasis">${match}</span>`);
+    return body;
+
   }
 }
