80f44431a89bf28d694fa773f2ec55fab989fe54
Kolton Bloom <75853497+bloomkd46@users.noreply.github.com>
2026-08-03 15:28:08 -0700

    switched the setup page to signals

--- projects/renderer/src/app/windows/setup/setup.component.ts
+++ projects/renderer/src/app/windows/setup/setup.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, signal, type OnInit, ChangeDetectionStrategy, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal, type OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppService } from '../../services/app/app.service';
 import { DialogService } from '../../services/dialog/dialog.service';
@@ -9,7 +9,7 @@ import { WindowService } from '../../services/window/window.service';
   selector: 'app-setup',
   templateUrl: './setup.component.html',
   styleUrl: './setup.component.scss',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
 export class SetupComponent implements OnInit {
@@ -19,51 +19,46 @@ export class SetupComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private appService = inject(AppService);
 
-  id = '';
-  token = '';
-  name = '';
-  email = '';
-  payloadId?: string;
-  payloadToken?: string;
-  payloadName?: string;
-  payloadEmail?: string;
-  existingID?: string;
-  existingToken?: string;
-  existingName?: string;
-  existingEmail?: string;
-  _cloning = false;
-  set cloning(cloning: boolean | undefined) {
-    if (cloning !== undefined) {
-      this._cloning = cloning;
-    }
-    this.windowService.setClosable(!cloning);
-  }
-  get cloning() {
-    return this._cloning;
+  id = signal('');
+  token = signal('');
+  name = signal('');
+  email = signal('');
+  payloadId = signal('');
+  payloadToken = signal('');
+  payloadName = signal('');
+  payloadEmail = signal('');
+  existingID = signal('');
+  existingToken = signal('');
+  existingName = signal('');
+  existingEmail = signal('');
+  cloning = signal(false);
+  constructor() {
+    effect(() => {
+      this.windowService.setClosable(!this.cloning());
+    });
   }
   dots = '';
   async setup() {
-    this.cloning = true;
+    this.cloning.set(true);
     try {
-      await this.gitService.setup(this.id || 'web-ui', this.name, this.email, this.token);
+      await this.gitService.setup(this.id() || 'web-ui', this.name(), this.email(), this.token());
     } catch (error) {
       await this.gitService.handleError('Clone Failed!', error as Error);
     } finally {
-      this.cloning = undefined;
-      setTimeout(this.windowService.close, 1000);
+
+      setTimeout(() => this.windowService.close(false, true), 1000);
     }
   }
   async updateSetup() {
-    this.cloning = true;
+    this.cloning.set(true);
     try {
-      await this.gitService.updateSetup(this.id, this.name, this.email, this.token);
+      await this.gitService.updateSetup(this.id(), this.name(), this.email(), this.token());
     } catch (error: unknown) {
-      const message = (error as { message: string }).message;
+      const message = (error as { message: string; }).message;
       await this.dialogService.showMessageBox({ message: 'Failed To Update!', type: 'error', detail: message.split('fatal:').pop() });
       console.error(error);
     } finally {
-      this.cloning = undefined;
-      this.windowService.close();
+      this.windowService.close(false, true);
     }
   }
   remoteProgress = signal(0);
@@ -95,35 +90,24 @@ export class SetupComponent implements OnInit {
       }
     }, 500);
     const setup = await this.gitService.getSetup();
-    this.existingName = setup.name;
-    this.existingEmail = setup.email;
-    this.existingID = setup.repo;
-    this.existingToken = setup.token;
+    this.existingName.set(setup.name || '');
+    this.existingEmail.set(setup.email || '');
+    this.existingID.set(setup.repo || '');
+    this.existingToken.set(setup.token || '');
     const payload = this.route.snapshot.queryParamMap.get('payload');
     if (payload) {
       // The payload is encrypted base64 JSON containing repo, token, name, and email. To decrypt all numbers are flipped (1 is 9, 2 is 8, etc)
       const decryptedPayload = payload.replace(/\d/g, (digit) => (9 - parseInt(digit)).toString());
       const urlPayload = JSON.parse(await this.appService.base64Decode(decryptedPayload));
-      this.payloadId = urlPayload.repo;
-      this.payloadToken = urlPayload.token;
-      this.payloadName = urlPayload.name;
-      this.payloadEmail = urlPayload.email;
+      this.payloadId.set(urlPayload.repo);
+      this.payloadToken.set(urlPayload.token);
+      this.payloadName.set(urlPayload.name);
+      this.payloadEmail.set(urlPayload.email);
     }
 
-    this.id = this.payloadId || (this.existingID == 'web-ui' ? '' : this.existingID) || '';
-    this.token = this.payloadToken || this.existingToken || '';
-    this.name = this.existingName || this.payloadName || '';
-    this.email = this.existingEmail || this.payloadEmail || '';
-  }
-  advanced = false;
-  @HostListener('document:keydown', ['$event']) handleKeydownEvent(event: KeyboardEvent) {
-    if (event.key === 'Alt') {
-      this.advanced = true;
-    }
-  }
-  @HostListener('document:keyup', ['$event']) handleKeyupEvent(event: KeyboardEvent) {
-    if (event.key === 'Alt') {
-      this.advanced = false;
-    }
+    this.id.set(this.payloadId() || (this.existingID() == 'web-ui' ? '' : this.existingID()) || '');
+    this.token.set(this.payloadToken() || this.existingToken() || '');
+    this.name.set(this.existingName() || this.payloadName() || '');
+    this.email.set(this.existingEmail() || this.payloadEmail() || '');
   }
 }
