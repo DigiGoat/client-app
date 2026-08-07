c37b3162b83275fd46224ff52e0004713b302744
Kolton Bloom <75853497+bloomkd46@users.noreply.github.com>
2026-08-07 11:05:54 -0700

    Finished signals migration for case input

--- projects/renderer/src/app/windows/goat/elements/case-input/case-input.component.ts
+++ projects/renderer/src/app/windows/goat/elements/case-input/case-input.component.ts
@@ -1,24 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, model, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-case-input',
   templateUrl: './case-input.component.html',
   styleUrl: './case-input.component.scss',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
 export class CaseInputComponent {
-  @Input() value?: string;
   @Output() valueChange = new EventEmitter<string>();
 
   signalValue = model<string>();
-
-  invertCase(index: number): void {
-    const char = this.value![index];
-    const newChar = char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
-    this.value = this.value!.slice(0, index) + newChar + this.value!.slice(index + 1);
-    this.valueChange.emit(this.value);
-  }
 
   invertSignalCase(index: number): void {
     const char = this.signalValue()![index];
